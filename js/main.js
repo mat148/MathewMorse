569d1f2a539de3768cf406198c3785ec4af999fd
Mathew Morse <mathewmorse@Mathews-MacBook-Pro-3.local>
2015-03-06 12:22:53 -0500

    v33

--- js/main.js
+++ js/main.js
@@ -11,6 +11,22 @@ function init()
     setTimeout(function(){
         $(title).addClass("second-line-anim");
     }, 150);
+
+    /*$(".work").hover(function(){
+        $(".work").css("background-color", "yellow");
+        },function(){
+        $(".work").css("background-color", "pink");
+    });*/
+    
+    /*$( ".work" ).hover(
+  function() {
+    $( this ).children(".project-container").css("top", "279px");
+  }, function() {
+    $( this ).children(".project-container").css("top", "313px");
+  }
+);*/
+ 
+
 }
 
 $(function() {
@@ -27,4 +43,3 @@ $(function() {
     }
   });
 });
- 
