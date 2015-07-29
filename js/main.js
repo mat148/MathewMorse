badaf54fbc3e50e6aa06c96dd0dea3bbff2ee5a4
Mathew Morse <mat.morse@matm-mbp.ff0000.com>
2015-07-28 17:50:22 -0700

    Slide out nav

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var height = $(window).height();
+var width = $(window).width();
     
 //console.log(height);
     
@@ -19,22 +20,16 @@ function init()
     setTimeout(function(){
         $(title).addClass("second-line-anim");
     }, 150);
-
-    /*$(".work").hover(function(){
-        $(".work").css("background-color", "yellow");
-        },function(){
-        $(".work").css("background-color", "pink");
-    });*/
     
-    /*$( ".work" ).hover(
-  function() {
-    $( this ).children(".project-container").css("top", "279px");
-  }, function() {
-    $( this ).children(".project-container").css("top", "313px");
-  }
-);*/
- 
+    $('.nav-btn').mousedown(function(){
+        $('.slide-out').css({'top':'0px','right':'0px'});
+        $('.slide-out ul').css({'left':'10%'});
+    });
 
+    $('.slide-out').mousedown( function(){
+        $('.slide-out').css({'top':'0px', 'right': - width+'px'});
+        $('.slide-out ul').css({'left':'30%'});
+    } );
 }
 
 $(function() {
