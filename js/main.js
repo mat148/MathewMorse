12280f3fe3e8a23a6aa56022bd8bc4ab223bcbe8
Mathew Morse <mat.morse@matm-mbp.ff0000.com>
2015-07-29 16:57:09 -0700

    navigation and work section update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var height = $(window).height();
 var width = $(window).width();
-var clicked = 0;
+
+var workWidth = $('.work-container').width();
+
+var clicked = 'false';
     
 //console.log(height);
     
@@ -21,26 +24,33 @@ function init()
     setTimeout(function(){
         $(title).addClass("second-line-anim");
     }, 150);
+    
+    $('.work-container').css({
+        'height': (workWidth)+'px',
+    });
 }
 
 function close()
 {
     $('.slide-out').css({'top':'0px', 'right': - width+'px'});
-    $('.slide-out ul').css({'left':'30%'});
+    $('.slide-out ul #aboutButton').css({'left':'100%'});
+    $('.slide-out ul #workButton').css({'left':'120%'});
+    $('.slide-out ul #resumeButton').css({'left':'140%'});
+    $('.slide-out ul #contactButton').css({'left':'160%'});
     $('.line1').css({'top':'0px', 'transform':'translateY(-50%) rotate(0deg)',});
     $('.line2').css({'opacity':'1',});
     $('.line3').css({'top':'100%', 'transform':'translateY(-50%) rotate(0deg)',});
-    clicked = 0;
+    clicked = 'false';
 }
 
 $('.nav-btn').on('click',function(){
-    if(clicked == 0){
+    if(clicked == 'false'){
         $('.slide-out').css({'top':'0px','right':'0px'});
-        $('.slide-out ul').css({'left':'10%'});
+        $('.slide-out ul li').css({'left':'50%'});
         $('.line1').css({'top':'50%', 'transform':'translateY(-50%) rotate(45deg)',});
         $('.line2').css({'opacity':'0',});
         $('.line3').css({'top':'50%', 'transform':'translateY(-50%) rotate(-45deg)',});
-        clicked = +1;
+        clicked = 'true';
     }
     else{
         close();
@@ -54,6 +64,15 @@ $('.slide-out').on('click', function(){
 $('.open').on('click', function(){
     alert('yo');
     close();
+});
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() >= (height-500)) {
+    $('nav').css({'background':'#2b333e',});
+  }
+    if ($(window).scrollTop() <= height) {
+    $('nav').css({'background':'transparent',});
+  }
 });
 
 $(function() {
