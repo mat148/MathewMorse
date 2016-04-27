4710a0d8608da4b39dafa270a23595e3ac980c36
mat148 <mrm1779@gmail.com>
2016-04-27 16:02:31 -0700

    Optimizing images and adjusting the hero javascript load

--- js/main.js
+++ js/main.js
@@ -4,11 +4,7 @@ var workWidth;
 
 var clicked = 'false';
 
-window.onload = init;
-        
-function init()
-{
-    if ($('.hero').length){
+if ($('.hero').length){
         if($('.hero').hasClass('project-page')){
             $(window).resize(function() {
                 $('.hero').height(($(window).height())/2);
@@ -20,10 +16,15 @@ function init()
             }).resize();
         }
     }
-    else{
-        $('nav').css({'background':'#2b333e', 'opacity':'0.98'});
-    }
-    
+else{
+    $('nav').css({'background':'#2b333e', 'opacity':'0.98'});
+}
+
+window.onload = init;
+     
+function init()
+{   
+    //blah
     workWidth = $('.work-container').width();
     $('.work-container').css({
         'height': (workWidth)+'px',
